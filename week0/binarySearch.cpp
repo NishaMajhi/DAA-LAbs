5d496322fae458534a505c79ae4f9c151da9b3bf
NishaMajhi <84006994+NishaMajhi@users.noreply.github.com>
2021-07-15 23:14:50 -0500

    Create binarySearch.cpp

---
+++ week0/binarySearch.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+int binarySearch(int arr[],int n,int key,int &c)
+{
+    int start=0;
+    int stop=n-1;
+    while(start<=stop)
+    {
+        int mid=(start+stop)/2;
+        if(key==arr[mid])
+        {
+            c++;
+            return 1;
+        }
+        else if(key>arr[mid])
+                start=mid+1;
+             else
+                stop=mid-1;
+        c++;
+    }
+    return -1;
+
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t>0)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        insertionSort(arr,n);
+        int key,cnt=0;
+        cin>>key;
+        int ans=binarySearch(arr,n,key,cnt);
+        if(ans==-1)
+            cout<<"Not Present "<<endl;
+        else
+            cout<<"present "<<cnt<<endl;
+        t--;
+    }
+    return 0;
+}
